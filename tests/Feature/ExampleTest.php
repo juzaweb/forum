0b06988321599401b44e4a731e4eb605aa334060
Thế Anh Đặng <dangtheanh16@gmail.com>
2026-02-13 16:18:04 +0700

    feat(forum): initialize package structure and testing environment with basic files and test setup

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace \Forum\Tests\Feature;
+
+use \Forum\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace \Forum\Tests\Unit;
+
+use \Forum\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_example(): void
+    {
+        $this->assertTrue(true);
+    }
+}
